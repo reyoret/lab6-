372782bfe09e21a8c4ef557c3ae71cc8f3834043
scm token <121426400+reyoret@users.noreply.github.com>
2026-05-12 11:34:49 +0300

    Set base path for Vite configuration

--- lab6/vite.config.ts
+++ lab6/vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    base: '/lab6-/',
     vue(),
     vueDevTools(),
   ],
